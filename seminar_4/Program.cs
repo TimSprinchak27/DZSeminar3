5fef3b6e1238ec00a6545eba475f9934f914c8d0
TIM27 <sprinchak27@mail.ru>
2022-07-16 23:02:30 +0300

    .added task 1 seminar

---
+++ seminar_4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+}
