d6783d450d45114f2452bbe936fbd4595eb953c2
TIM27 <sprinchak27@mail.ru>
2022-07-17 10:46:41 +0300

    .added task 27 seminar 4

---
+++ task_27_DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+namespace Task27
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            Console.Write("Введите число: ");
+            int i = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (i > 0)
+            {
+                sum = sum + i %10;
+                i = i / 10;
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+
+}
