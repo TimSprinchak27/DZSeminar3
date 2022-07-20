e687a5a668d3ad91cef57563e31c95cf85402f74
TIM27 <sprinchak27@mail.ru>
2022-07-20 06:29:28 +0300

    .added task 34 stminar 5

--- task_34_DZ/Program.cs
+++ task_34_DZ/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+﻿// Задайте одномерный массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
@@ -8,8 +8,39 @@ namespace Task34
     {
         public static void Main ()
         {
-            int [] array = new int [4];
+           Console.WriteLine("Задайте размер массива");
+           int size = Convert.ToInt32(Console.ReadLine());
+           int[] numbers = new int[size];
+           FillArrayRandomNumbers(numbers);
+           Console.WriteLine("Вот наш массив: ");
+           PrintArray(numbers);
+           int count = 0;
+
+           for (int i = 0; i < numbers.Length; i++)
+           if (numbers[i] % 2 == 0)
+           count++;
+           Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
             
         }
+        static void FillArrayRandomNumbers(int[] numbers)
+        {
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new Random().Next(100,1000);
+            }
+        }
+        static void PrintArray(int[] numbers)
+        {
+            Console.Write("[ ");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+
+            }
+            Console.Write("]");
+        }   
+
     }
+        
 }
