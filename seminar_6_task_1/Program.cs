c65f405e9b52d8ce7684bae7cfef9cf7c6a7aed6
TIM27 <sprinchak27@mail.ru>
2022-07-30 13:28:15 +0300

    .added task 1 seminar 6

---
+++ seminar_6_task_1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая перевернет одномерный масив (последний элемент будет на первом месте, а первый на последенем)
+//[1 2 3 4 5] ->[5 4 3 2 1]
+// [7 6 3 6] -> [6 3 7 6]
+
+public class ProgSeminar
+{
+    public int[] GetArray()
+    {
+        Random random = new Random();
+        int x = random.Next(5, 11);
+        int[] arr = new int[x];
+
+        Console.WriteLine("Вводим массив: ");
+
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(0, 41);
+            Console.Write(arr[i] + " ");
+        }
+
+        return arr;
+    }
+
+    public static void Main()
+    {
+        ProgSeminar progSeminar = new ProgSeminar();
+        int[] mass = progSeminar.GetArray();
+        for (int i = 0; i < mass.Length / 2; i++)
+        {
+            int a = mass[i];
+            mass[i] = mass[mass.Length - 1 - i];
+            mass[mass.Length - 1 - i]= a;
+            Console.WriteLine(" ");
+
+        }
+       
+        Console.WriteLine("Выводим получившийся массив: ");
+
+        for (int i = 0; i < mass.Length; i++)
+        {
+            Console.Write(mass[i] + " ");
+        }
+
+
+    }
+}
