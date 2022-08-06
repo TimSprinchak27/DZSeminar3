c89a689eb7cdf16b328a4c7f705c17affd96d54f
TIM27 <sprinchak27@mail.ru>
2022-08-06 11:53:48 +0300

    .added task 68 seminar 9

---
+++ task_68_DZ/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task68
+{
+    public class Program
+    // Напишите программу вычисления функции Аккермана с помощью рекурсии.
+    // Даны два неотрицательных числа m и n.
+    // m = 2, n = 3 -> A(m,n) = 29
+
+    {
+        static void Main(string[] args)
+        {
+            int m = 2, n = 3;
+            static int A(int n, int m)
+            {
+                if (n == 0)
+                    return m + 1;
+                else
+                  if ((n != 0) && (m == 0))
+                    return A(n - 1, 1);
+                else
+                    return A(n - 1, A(n, m - 1));
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"Результат вычисления финкции Аккермана для заданных знечений = {A(n, m)}" + "\n");
+
+        }
+
+    }
+}
