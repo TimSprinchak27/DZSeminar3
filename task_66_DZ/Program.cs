805b6d614edef982a64f92f75c5c593a02d6836e
TIM27 <sprinchak27@mail.ru>
2022-08-06 11:49:29 +0300

    .added task 66 seminar 9

---
+++ task_66_DZ/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Homework_9
+{
+    public class task_66
+    // Задайте значения M и N. Напишите программу,
+    // которая найдёт сумму натуральных элементов в промежутке от M до N
+    // M = 1; N = 15 -> 120
+    // M = 4; N = 8. -> 30
+    {
+        static void Main(string[] args)
+        {
+            int rez = 0;
+            Console.Write("Введите M: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите N: ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"M={M}, N={N}");
+            Console.WriteLine();
+            if (M > N)
+            {
+                for (int i = N; i <= M; i++)
+                {
+                    rez += i;
+                    Console.Write($" {i}");
+                }
+            }
+            else
+            {
+                for (int i = M; i <= N; i++)
+                {
+                    rez += i;
+                    Console.Write($" {i}");
+                }
+            }
+            Console.WriteLine();
+            Console.Write($"Сумма натуральных элементов в заданном промежуке = {rez}");
+        }
+    }
+}
